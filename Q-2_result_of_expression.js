4c6623794ad1f00a067512f186a9af6aa7a7990c
Hemaxipatel <121233551+Hemaxipatel@users.noreply.github.com>
2023-07-10 18:05:44 +0530

    Add files via upload

---
+++ Q-2_result_of_expression.js
@@ -0,0 +1,15 @@
+//what will be the result for these expression
+// 1. 5 > 4
+Ans: true
+// 2. "apple" > "pineapple"
+Ans: False
+// 3. "2" > "12"
+Ans: true
+// 4. undefined == null
+Ans: true
+// 5. undefined === null
+Ans: false
+// 6. null == "\n0\n"
+Ans: false
+// 7. null === +"\n0\n"
+Ans: false
